6da35957e5bab7b5b354606fe133137c7b80c5d0
Hiren <hchandegara@gasleaksensors.com>
2024-05-02 15:55:34 +0530

    removed examples of freeRTOS from this branch

--- FREERTOS/001_Task/Debug/ThirdParty/Segger/Segger/Syscalls/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,6 +0,0 @@
-ThirdParty/Segger/Segger/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
- ../ThirdParty/Segger/Segger/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
- D:/Non-Project/FREERTOS/001_Task/ThirdParty/Segger/Segger/SEGGER_RTT.h \
- D:/Non-Project/FREERTOS/001_Task/ThirdParty/Segger/Config/SEGGER_RTT_Conf.h
-D:/Non-Project/FREERTOS/001_Task/ThirdParty/Segger/Segger/SEGGER_RTT.h:
-D:/Non-Project/FREERTOS/001_Task/ThirdParty/Segger/Config/SEGGER_RTT_Conf.h:
